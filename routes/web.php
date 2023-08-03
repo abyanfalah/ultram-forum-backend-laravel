1ad48f163019deebc565d854357e19b7c552fb6f
abyanfalah <abyanf410@gmail.com>
2023-08-03 18:44:10 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+	return ['Laravel' => app()->version()];
+});
+
+Route::post('/test', function (Request $request) {
+	return [
+		'request' => 'ddaaang',
+		'msg' => 'OK',
+	];
+});
+
+Route::post('/login', function (Request $request) {
+	return $request;
+});
+
+require __DIR__ . '/auth.php';
