9e52034dcc0d3254aa3de6de983e4cfe2e0d0ea5
abyanfalah <abyanf410@gmail.com>
2023-09-10 20:09:27 +0700

    follow back

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -59,7 +59,7 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->bio = $request->bio;
         $user->save();
-
+        $user->withFollowDetails();
         return $user;
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,103 +20,110 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+	use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'password',
-    // ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	// protected $fillable = [
+	//     'name',
+	//     'email',
+	//     'password',
+	// ];
 
-    protected $guarded = ['id'];
+	protected $guarded = ['id'];
 
-    protected $with = [
-        // 'isFollowed'
-    ];
+	protected $with = [
+		// 'isFollowed'
+	];
 
 
-    protected $withCount = [
-        // 'followees',
-        // 'followers',
-        // 'isFollowed',
-    ];
+	protected $withCount = [
+		// 'followees',
+		// 'followers',
+		// 'isFollowed',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+		'password' => 'hashed',
+	];
 
-    public function threads(): HasMany
-    {
-        return $this->hasMany(Thread::class);
-    }
+	public function threads(): HasMany
+	{
+		return $this->hasMany(Thread::class);
+	}
 
-    public function threadReactions(): HasMany
-    {
-        return $this->hasMany(ThreadReaction::class);
-    }
+	public function threadReactions(): HasMany
+	{
+		return $this->hasMany(ThreadReaction::class);
+	}
 
-    public function posts(): HasMany
-    {
-        return $this->hasMany(Post::class);
-    }
+	public function posts(): HasMany
+	{
+		return $this->hasMany(Post::class);
+	}
 
-    public function postReactions(): HasMany
-    {
-        return $this->hasMany(PostReaction::class);
-    }
+	public function postReactions(): HasMany
+	{
+		return $this->hasMany(PostReaction::class);
+	}
 
-    public function followers()
-    {
-        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
-    }
+	public function followers()
+	{
+		return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+	}
 
-    public function followees()
-    {
-        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
-    }
+	public function followees()
+	{
+		return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+	}
 
-    public function isFollowed()
-    {
-        return $this->hasMany(Follower::class, 'user_id')
-            ->where('follower_id', auth()->user()->id)->exists();
-    }
+	public function isFollowed()
+	{
+		return $this->hasMany(Follower::class, 'user_id')
+			->where('follower_id', auth()->user()->id)->exists();
+	}
 
-    public function conversationParticipations()
-    {
-        return $this->hasMany(ConversationParticipant::class, 'user_id');
-    }
+	public function isFollowing()
+	{
+		return $this->hasMany(Follower::class, 'follower_id')
+			->where('user_id', auth()->user()->id)->exists();
+	}
 
-    public function conversations()
-    {
-        return $this->belongsToMany(Conversation::class, 'conversation_participants');
-    }
+	public function conversationParticipations()
+	{
+		return $this->hasMany(ConversationParticipant::class, 'user_id');
+	}
 
-    public function withFollowDetails()
-    {
-        $this->followers_count = $this->followers()->count();
-        $this->followees_count = $this->followees()->count();
-        $this->is_followed = $this->isFollowed();
-        return $this;
-    }
+	public function conversations()
+	{
+		return $this->belongsToMany(Conversation::class, 'conversation_participants');
+	}
+
+	public function withFollowDetails()
+	{
+		$this->followers_count = $this->followers()->count();
+		$this->followees_count = $this->followees()->count();
+		$this->is_followed = $this->isFollowed();
+		$this->is_following = $this->isFollowing();
+		return $this;
+	}
 }
